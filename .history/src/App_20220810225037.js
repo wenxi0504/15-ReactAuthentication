d6dca7d33f41bb0d887baf2b3072c14872535eb9
Sophie <wenxi1988@hotmail.com>
2022-08-10 22:53:25 -0400

    Protecting frontend pages

--- .history/src/App_20220810225037.js
+++ .history/src/App_20220810225037.js
@@ -20,12 +20,11 @@ function App() {
             <AuthPage />
           </Route>
         )}
-
-        <Route path="/profile">
-          {authCtx.isLoggedIn && <UserProfile />}
-          {!authCtx.isLoggedIn && <Redirect to="/auth" />}
-        </Route>
-
+        {authCtx.isLoggedIn && (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
         <Route path="*">
           <Redirect to="/" />
         </Route>
